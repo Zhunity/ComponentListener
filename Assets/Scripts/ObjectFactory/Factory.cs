74142e10185379e03da2251afede03babba1fb40
lizh <poinseting@163.com>
2020-03-27 19:47:38 +0800

    添加Component监听

---
+++ Assets/Scripts/ObjectFactory/Factory.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[InitializeOnLoad]
+public class Factory
+{
+	static Factory()
+	{
+		ObjectFactory.componentWasAdded += ComponentWasAdded;
+	}
+
+	static public void ComponentWasAdded(Component com)
+	{
+		Debug.Log(com);
+	}
+}
